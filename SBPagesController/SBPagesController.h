5ef9dfea449d428b99cf27b4917a0a1392b0c9dc
salimbraksa <salim.braksa@gmail.com>
2015-12-10 14:04:05 +0000

    Initial Commit

---
+++ SBPagesController/SBPagesController.h
@@ -0,0 +1,19 @@
+//
+//  SBPagesController.h
+//  SBPagesController
+//
+//  Created by Salim Ive on 12/10/15.
+//  Copyright © 2015 braksa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBPagesController.
+FOUNDATION_EXPORT double SBPagesControllerVersionNumber;
+
+//! Project version string for SBPagesController.
+FOUNDATION_EXPORT const unsigned char SBPagesControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBPagesController/PublicHeader.h>
+
+
